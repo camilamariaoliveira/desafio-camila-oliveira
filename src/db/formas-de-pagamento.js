8a026ba5e7a950eb8a6553e5adf2d03b75b7afdf
Camila Oliveira <camilamoliveiratech@gmail.com>
2023-08-16 22:53:05 -0300

    feat: Adicionando arquivos de dados sobre formas de pagamentos e cardapio

---
+++ src/db/formas-de-pagamento.js
@@ -0,0 +1,18 @@
+const formasDePagamentoETaxa = [
+    {
+        tipo: "debito",
+        taxa: 1
+    },
+    {
+        tipo: "credito",
+        taxa: 1.03
+    },
+    {
+        tipo: "dinheiro",
+        taxa: 0.95
+    }
+];
+
+export {
+    formasDePagamentoETaxa
+}
